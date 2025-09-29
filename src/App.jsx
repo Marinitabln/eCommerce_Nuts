3a8123100d538f12085def63c9f4f420cb677b77
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-09-29 14:37:54 -0300

    Agrego data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import './App.css'
+import { useState } from 'react'
+import Cart from './components/cart/Cart'
 import ProductsContainer from './components/ProductsContainer/ProductsContainer'
 
-
-
+const CART = [
+  {
+    product_title: "Nueces",
+    presentation: "250gr",
+    quantity: 2,
+    unitPrice: "1000",
+    totalPrice: "2000"
+  },
+  {
+    product_title: "Pistachos",
+    presentation: "500gr",
+    quantity: 3,
+    unitPrice: "1900",
+    totalPrice: "5700"
+  }
+]
 
 function App() {
+
+  const [cart, setCart] = useState(CART)
+
+  //Add product to cart
+  const handleAddToCart = (productToCart) => {
+    setCart([...cart, productToCart])
+  }
+
+  console.log(cart);
+
+
+  //Clear cart
+  const handleClearCart = () => {
+    setCart([])
+  }
+
   return (
     <>
-      <ProductsContainer />
+      <ProductsContainer handleAddToCart={handleAddToCart} />
+      <Cart cart={cart} handleClearCart={handleClearCart}  />
     </>
   )
 }
--- src/data/constants.jsx
+++ src/data/constants.jsx
@@ -3,24 +3,35 @@ export const PRODUCTS = [
     url_img: "https://www.pistepistachos.com/_img/productos-pistacho-cascara.jpg",
     title: "Pistacho",
     description: "Con la sal justa y necesaria. El snack perfecto para el picoteo con amigos.",
-    price: "$1000 - $5000"
+    presentations: ["250gr", "500gr", "1kg"],
+    price: ["1000", "1900", "3700"]
   },
   {
     url_img: "https://img.freepik.com/fotos-premium/close-up-nueces-cascara-mercado-agricultores-turquia_501761-1643.jpg",
     title: "Nueces",
     description: "Con la sal justa y necesaria. El snack perfecto para el picoteo con amigos.",
-    price: "$1000 - $5000"
+    presentations: ["250gr", "500gr", "1kg"],
+    price: ["1000", "1900", "3700"]
   },
   {
-    url_img: "https://img.freepik.com/fotos-premium/close-up-nueces-cascara-mercado-agricultores-turquia_501761-1643.jpg",
-    title: "Nueces",
+    url_img: "https://acdn-us.mitiendanube.com/stores/001/129/542/products/pasas-jumbo1-b46880933fb09a825c15850889915504-1024-1024.jpg",
+    title: "Pasas negras",
     description: "Con la sal justa y necesaria. El snack perfecto para el picoteo con amigos.",
-    price: "$1000 - $5000"
+    presentations: ["250gr", "500gr"],
+    price: ["1000", "1900"]
   },
   {
-    url_img: "https://img.freepik.com/fotos-premium/close-up-nueces-cascara-mercado-agricultores-turquia_501761-1643.jpg",
-    title: "Nueces",
+    url_img: "https://i0.wp.com/latiendadeidoya.com/wp-content/uploads/2023/01/ARANDANOS-ROJOS-SIN-AZUCAR.jpg",
+    title: "Arándanos deshidratados",
     description: "Con la sal justa y necesaria. El snack perfecto para el picoteo con amigos.",
-    price: "$1000 - $5000"
+    presentations: ["250gr", "500gr", "1kg"],
+    price: ["1000", "1900", "3700"]
+  },
+  {
+    url_img: "https://www.infocampo.com.ar/wp-content/uploads/2019/02/foto_duraznos_deshidratados.jpg",
+    title: "Duraznos deshidratados",
+    description: "Con la sal justa y necesaria. El snack perfecto para el picoteo con amigos.",
+    presentations: ["250gr", "500gr", "1kg"],
+    price: ["1000", "1900", "3700"]
   }
 ]
