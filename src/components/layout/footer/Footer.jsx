b21edf50d858c34c65383b1dbe990bdf1d2f692b
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-10-02 12:31:05 -0300

    Agrego componente Footer

---
+++ src/components/layout/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Footer.module.css'
+import { FaWhatsapp } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.contact}>
+          <h4>Contactanos</h4>
+          <FaWhatsapp size={30} color='#fff' />
+          <a href=''>+54 9 2612 56-0314</a>
+          <a href=''>+54 9 2615 54-6708</a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
