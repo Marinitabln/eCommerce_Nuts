793156d91d433051b9b8ff218f2aac0738c98fcf
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-10-07 10:51:36 -0300

    logica y estilos carrito

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,34 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Cart.module.css'
 
 const Cart = ({ cart, handleClearCart }) => {
 
-    //Total amount cart
-    /* const total = cart.reduce((sum, item) => sum + item.totalPrice) */
+    
+    //Total Carrito
+    const [total, setTotal] = useState(0)
+
+
+useEffect(()=>{
+    
+    /* setTotal(cart.reduce((sum, item) => sum + Number(item.subtotal))) : ""
+     */
+
+    cart.length > 0 ? (
+        cart.map((item)=>{
+            setTotal(prevTotal => prevTotal + Number(item.subtotal))
+        } )
+    ):""  
+
+},[cart])
 
 
     return (
 
         <section className={styles.cart}>
-            <h1>Carrito de compras</h1>
-            {cart.length === 0 ? (
-                <p>Su carrito está vacío</p>)
-                :
-                <>
-                    < ul className={styles.cartContainer} >
-                        {(cart.map((item, index) => {
-                            console.log({ item });
-                            return (
-                                <li key={index}>{item.product_title}</li>
-                            )
-                        }
-                        ))}
-                        <button onClick={handleClearCart}>Vaciar carrito</button>
-                    </ul >
+            <div className={styles.container}>
+                <h1>Carrito de compras</h1>
+                {cart.length === 0 ? (
+                    <p>Su carrito está vacío</p>)
+                    :
+                    <>
+                        < ul className={styles.cartContainer} >
+                         <li className={styles.cartItem}>
+                                        <p>PRODUCTO</p>
+                                        <p></p>
+                                        <p>CANT.</p> 
+                                        <p>P.UNIT.</p> 
+                                        <p>SUBTOTAL</p>                                         
+                                    </li>
+                            {(cart.map((item, index) => {
+                                console.log(Number(item.subtotal));                                
+                                return (
+                                    <li key={index} className={styles.cartItem}>
+                                        <span>{item.product_name}</span>
+                                        <span>{item.presentation}</span>
+                                        <span>{item.quantity}</span> 
+                                        <span>{item.unitPrice}</span> 
+                                        <span>{item.subtotal}</span>                                         
+                                    </li>                                     
+                                )
+                            }
+                            ))}
+                            <span>{total}</span>
+                            <button onClick={handleClearCart}>Vaciar carrito</button>
+                        </ul >
 
-                </>
-            }
-
+                    </>
+                }
+            </div>
         </section>
     )
 }
