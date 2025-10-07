4b4808d2092d8d603356116d308c102f2d29cbe5
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-10-07 10:53:55 -0300

    Implementacion de layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,35 @@ import './App.css'
 import { useState } from 'react'
 import Cart from './components/cart/Cart'
 import ProductsContainer from './components/ProductsContainer/ProductsContainer'
+import LayoutGral from './components/layout/LayoutGral'
 
 const CART = [
   {
-    product_title: "Nueces",
+    product_name: "Nueces",
     presentation: "250gr",
     quantity: 2,
     unitPrice: "1000",
-    totalPrice: "2000"
+    subtotal: "2000"
   },
   {
-    product_title: "Pistachos",
+    product_name: "Pistachos",
     presentation: "500gr",
     quantity: 3,
     unitPrice: "1900",
-    totalPrice: "5700"
+    subtotal: "5700"
   }
 ]
 
 function App() {
 
-  const [cart, setCart] = useState(CART)
+  const [cart, setCart] = useState([])
 
   //Add product to cart
   const handleAddToCart = (productToCart) => {
-    setCart([...cart, productToCart])
+    setCart((prevCart)=>[...prevCart, productToCart])
   }
 
-  console.log(cart);
-
+/*   console.log(cart); */
 
   //Clear cart
   const handleClearCart = () => {
@@ -38,10 +38,12 @@ function App() {
   }
 
   return (
-    <>
+
+    <LayoutGral>
       <ProductsContainer handleAddToCart={handleAddToCart} />
-      <Cart cart={cart} handleClearCart={handleClearCart}  />
-    </>
+      <Cart cart={cart} handleClearCart={handleClearCart} />
+    </LayoutGral>
+
   )
 }
 
---
+++ src/components/layout/LayoutGral.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './header/Header'
+import Footer from './footer/Footer'
+
+
+const LayoutGral = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default LayoutGral
